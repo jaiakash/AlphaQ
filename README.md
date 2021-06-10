6111d45c92bdd6c9843078f6d209bcf322055c08
Akash Jaiswal <33419526+jaiakash@users.noreply.github.com>
2021-06-11 02:52:42 +0530

    Fixed git error in script file

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # AlphaQ
 SysAd. Alpha Q is shifting all it’s management tasks online! As a system administrator, you are tasked with setting up the basic system for the group. 
 
-<<<<<<< HEAD
 Submitted by Akash Jaiswal Roll no 103120007
-=======
-Submitted by Akash Jaiswal Roll no 103120007
->>>>>>> 96fdc5d... Added permit fucntion (persmission command left)
--- script.sh
+++ script.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-#function to create users
-function genUser{
-    for i in {01..30}
-    do
-        sudo adduser $1_$i
-        #adduser automatically creates home directory
-        #unlike useradd
-
-        #Adding to group
-        if [$i -gl 11]
-        then
-            sudo adduser $1_$i secondyr
-        elif [$i -gl 21]
-        then
-            sudo adduser $1_$i thirdyr
-        elif [$i -gl 31]
-        then
-            sudo adduser $1_$i fourthyr
-        fi
-=======
 #function to create users and add to domain gorup
 function genUser{
     for i in {01..30}
@@ -28,7 +7,6 @@ function genUser{
         sudo adduser $1_$i $1
         #adduser automatically creates home directory
         #unlike useradd
->>>>>>> 96fdc5d... Added permit fucntion (persmission command left)
     done
 }
 
@@ -40,16 +18,6 @@ function schedule{
     done
 }
 
-<<<<<<< HEAD
-#Creating User Group
-newgrp head
-newgrp fourthyr
-newgrp thirdyr
-negrp secondyr
-
-#Adding Jay_Jay and users of 3 domains
-sudo adduser jay_jay
-=======
 #function to copy schedule file to each user's directory
 # function permit{
 #     for i in {01..30}
@@ -78,16 +46,10 @@ negrp webdev
 
 #Adding Jay_Jay and users of 3 domains
 sudo adduser jay_jay alpha
->>>>>>> 96fdc5d... Added permit fucntion (persmission command left)
 genUser sysad
 genUser appdev
 genUser webdev
 
-<<<<<<< HEAD
-#adding users to group
-# Done in genUser fucntion
-sudo adduser jay_jay head
-=======
 #Permission group
 newgrp head
 newgrp fourthyr
@@ -99,7 +61,6 @@ sudo adduser jay_jay head
 permit sysad
 permit appdev
 permit webdev
->>>>>>> 96fdc5d... Added permit fucntion (persmission command left)
 
 #Shedule
 cp future.txt /home/jay_jay/schedule.txt
