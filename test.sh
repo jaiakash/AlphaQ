77c09dbaff7cb518bcb42e47ebf70eb7b81ab0b6
Akash Jaiswal <akashjaiswal3846@gmail.com.com>
2021-06-13 03:51:42 +0530

    Completed genMoM

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@
 #./attendance.sh > ut.txt
 
 
+#getting 1st occurance line no of the date1
+start=$(grep -n $date1 attendance.log | head -1 | cut -d':' -f 1)
+#getting last occurance line no of the date2
+end=$(grep -n $date1 attendance.log | tail -1 | cut -d':' -f 1)
 
-date1=$1
-date2="2019-11-27"
-if [[ "$date1" == "" ]]
-then
-    echo Hi
-fi
+echo $start
+echo $end
